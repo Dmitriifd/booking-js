3c8683f95aa4ca64989b9a4bfb8eaf7a6664603a
mitriifd <idimka9@gmail.com>
2022-05-07 17:40:40 +0500

    part 1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
 import start from "./modules/start.js";
+import getFormPerson from "./modules/formPerson.js";
 
 const init = (selectorApp, title) => {
     const app = document.querySelector(selectorApp);
-    start(app, title);
-}
+    const {main, firstForm} = start(app, title);
 
+    firstForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const forms = getFormPerson(firstForm.count.value);
+        console.log(firstForm.count);
+        firstForm.remove();
+
+        main.append(...forms);
+    });
+}
 
 
 init('.app', 'Выберите тур');
